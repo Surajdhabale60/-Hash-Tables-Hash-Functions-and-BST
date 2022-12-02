d73755c20b4c0df0b3e7cec5b63430efa2ceb73d
Suraj Dhabale <surajdhabale60@gmail.com>
2022-12-02 22:26:19 +0530

    [Suraj]Add:Greeting Message

---
+++ HashTAableProject/HashTAableProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HashTableAndBSTProblems
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Hash table And Binary Search Tree(BST)");
+        }
+    }
+}
