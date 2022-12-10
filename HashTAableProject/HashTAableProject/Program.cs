ea91b6479921c9fb8f7858386cd7ce0f7d103449
Suraj Dhabale <surajdhabale60@gmail.com>
2022-12-10 09:10:39 +0530

    [Suraj]Add:Hash Table And Binary Search Tree

--- HashTAableProject/HashTAableProject/Program.cs
+++ HashTAableProject/HashTAableProject/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace HashTableAndBSTProblems
+﻿using HashTAableProject;
+
+namespace HashTableAndBSTProblems
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Hash table And Binary Search Tree(BST)");
+
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(6);
+            hash.Add("0", "to");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+            string hash5 = hash.Get("5");
+            string hash3 = hash.Get("3");
+
+            Console.WriteLine("Value of key 5 is :" + " " + hash5);
+            Console.WriteLine("Value of key 2 is :" + " " + hash3);
         }
     }
 }
